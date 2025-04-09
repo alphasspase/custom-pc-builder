0ea39780877c1bbb1a3bf139312530a6c4462acb
sameersultan41 <sameer@linkedunion.com>
2025-04-10 01:06:00 +0500

    create navbar pages

---
+++ src/app/build/page.tsx
@@ -0,0 +1,10 @@
+import ComingSoon from '@/components/global/ComingSoon'
+import React from 'react'
+
+const buildPage = () => {
+  return (
+    <div><ComingSoon/></div>
+  )
+}
+
+export default buildPage
---
+++ src/app/support/page.tsx
@@ -0,0 +1,12 @@
+import ComingSoon from '@/components/global/ComingSoon';
+import React from 'react';
+
+const supportPage = () => {
+  return (
+    <div>
+      <ComingSoon />{' '}
+    </div>
+  );
+};
+
+export default supportPage;
