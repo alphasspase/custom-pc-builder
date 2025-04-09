9895b4eee858d962f4fa1843ea8540536799ee46
sameersultan41 <sameer@linkedunion.com>
2025-04-09 18:17:55 +0500

    logo fix

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,22 +1,22 @@
-import { BenefitsSection } from './_components/benefits';
-import { CommunitySection } from './_components/community';
-import { ContactSection } from './_components/contact';
-import { FAQSection } from './_components/faq';
-import { FeaturesSection } from './_components/features';
 import { FooterSection } from './_components/footer';
 import { HeroSection } from './_components/hero';
-import { PricingSection } from './_components/pricing';
-import { ServicesSection } from './_components/services';
-import { SponsorsSection } from './_components/sponsors';
-import { TeamSection } from './_components/team';
-import { TestimonialSection } from './_components/testimonial';
+// import { BenefitsSection } from './_components/benefits';
+// import { CommunitySection } from './_components/community';
+// import { ContactSection } from './_components/contact';
+// import { FAQSection } from './_components/faq';
+// import { FeaturesSection } from './_components/features';
+// import { PricingSection } from './_components/pricing';
+// import { ServicesSection } from './_components/services';
+// import { SponsorsSection } from './_components/sponsors';
+// import { TeamSection } from './_components/team';
+// import { TestimonialSection } from './_components/testimonial';
 
 export default function Home() {
   return (
     <div>
       <>
         <HeroSection />
-        <SponsorsSection />
+        {/* <SponsorsSection />
         <BenefitsSection />
         <FeaturesSection />
         <ServicesSection />
@@ -25,7 +25,7 @@ export default function Home() {
         <CommunitySection />
         <PricingSection />
         <ContactSection />
-        <FAQSection />
+        <FAQSection /> */}
         <FooterSection />
       </>
     </div>
